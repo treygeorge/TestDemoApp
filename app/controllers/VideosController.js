b2784f6b42449e50c2021d207cef185f4358fc26
trey <treygeorge@gmail.com>
2014-10-13 11:03:49 -0500

    Fetches video data from Vimeo.com now

--- app/controllers/VideosController.js
+++ app/controllers/VideosController.js
@@ -1,9 +1,12 @@
 'use strict';
 
-angular.module('TestDemoApp').controller('VideosController', ['$scope', '$http', function($scope, $http) {
+angular.module('TestDemoApp').controller('VideosController', ['$scope', '$http', 'USER_TOKEN', function($scope, $http, USER_TOKEN) {
 
-    $http.get('/data/videos.json')
-        .success(function(data) {
-            $scope.videos = data;
-        });
+    $http.defaults.headers.common.Authorization = 'bearer ' + USER_TOKEN;
+
+    $http.get('https://api.vimeo.com/groups/275943/videos?page=1&per_page=50')
+    .success(function(data) {
+        console.log('videos for group 275943', data);
+    });
+    
 }]);
--- app/module.js
+++ app/module.js
@@ -1,3 +1,7 @@
 'use strict';
 
 var app = angular.module('TestDemoApp', ['ngRoute']);
+
+app.factory('USER_TOKEN', function() {
+    return '591062aa6445e80e7e0c89f86e14c7e9';
+});
