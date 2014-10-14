b39c53a43234be2273e1fa53153d81fed6bca5d9
trey <treygeorge@gmail.com>
2014-10-14 08:47:33 -0500

    Fixes tests.  Changes order of tasks to put video test towards the end.

--- test/unit/controllers/FizzBuzzControllerSpec.js
+++ test/unit/controllers/FizzBuzzControllerSpec.js
@@ -21,14 +21,14 @@ describe("FizzBuzzController", function() {
         //
         //     scope.fizzBuzz(15);
         //
-        //     expect(scope.fizzBuzzOutput).toEqual('FizzBuzz12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz');
+        //     expect(scope.fizzBuzzOutput).toEqual('12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz');
         // });
         //
         // it("should have the correct string for 30 integers", function() {
         //
         //     scope.fizzBuzz(30);
         //
-        //     expect(scope.fizzBuzzOutput).toEqual('FizzBuzz12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FizzBuzz');
+        //     expect(scope.fizzBuzzOutput).toEqual('12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829FizzBuzz');
         // });
     });
 });
