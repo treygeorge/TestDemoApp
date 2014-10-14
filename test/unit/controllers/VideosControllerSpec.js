86c542b76080b79240203fc7d8dea931446da9a2
trey <treygeorge@gmail.com>
2014-10-14 08:51:41 -0500

    Removes some functionality from the videos controller so that the person under test can implement the functionionality.

--- test/unit/controllers/VideosControllerSpec.js
+++ test/unit/controllers/VideosControllerSpec.js
@@ -15,11 +15,11 @@ describe('VideosController', function() {
         });
     }));
 
-    it("should set scope.videos on instantiation", inject(function($httpBackend) {
-        $httpBackend.expectGET('https://api.vimeo.com/groups/275943/videos?page=1&per_page=50').respond([{}]);
-
-        $httpBackend.flush();
-
-        expect(scope.videos).toBeDefined();
-    }));
+    // it("should set scope.videos on instantiation", inject(function($httpBackend) {
+    //     $httpBackend.expectGET('https://api.vimeo.com/groups/275943/videos?page=1&per_page=50').respond([{}]);
+    //
+    //     $httpBackend.flush();
+    //
+    //     expect(scope.videos).toBeDefined();
+    // }));
 });
