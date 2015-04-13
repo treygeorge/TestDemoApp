35054f6b6c9ce57ec3c88b8d2474e70d17a614c0
treygeorge <treygeorge@carfax.com>
2015-04-13 10:00:31 -0500

    initial commit

--- app/controllers/FizzBuzzController.js
+++ app/controllers/FizzBuzzController.js
@@ -3,6 +3,26 @@
 angular.module('TestDemoApp').controller('FizzBuzzController', ['$scope', function($scope) {
 
     $scope.fizzBuzz = function(numInts) {
+        var fizzBuzzOutput = '';
+        for(var i=1; i<=numInts; i++) {
+            if(i % 3 == 0 && i % 5 ==0){
+
+                fizzBuzzOutput += "FizzBuzz";
+            }
+            else if (i % 3 == 0) {
+                fizzBuzzOutput += "Fizz";
+            }
+            else if(i % 5 == 0)
+            {
+                fizzBuzzOutput += "Buzz";
+            }
+            else
+            {
+                fizzBuzzOutput += i;
+            }
+        }
+
+        $scope.fizzBuzzOutput = fizzBuzzOutput;
     };
 
     $scope.fizzBuzz(15);
